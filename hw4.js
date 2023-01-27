fc8f1f39ef5ebe016dbb99272237a5db789413b8
SaraLeya <kolesnichenkova_@mail.ru>
2023-01-27 14:45:51 +0300

    new file:   hw4.css new file:   hw4.css.map new file:   hw4.html new file:   hw4.js new file:   hw4.scss modified:   index1.html

---
+++ hw4.js
@@ -0,0 +1,31 @@
+/*2.3 */
+/*Задание 1*/
+/*
+let password = 'пароль';
+let userAnswer = prompt('Введите пароль');
+if (userAnswer === password){ 
+    alert('Пароль введен верно');
+} else {
+    alert('Пароль введен неверно');
+}
+/*Задание 2*/
+/*
+let c = prompt('запишите в нее любое число');
+if (c>0 && c<10) { 
+    alert('yes');
+} else {
+    alert('no');
+}
+/*Задание 3*/
+/*
+let d = 22;
+let c = 101;
+if (d>100 || c>100); {
+    alert('yes');
+}else {
+    alert('no');
+}
+/*Задание 4*/
+let a = 2;
+let b = 3;
+alert (a + b);
